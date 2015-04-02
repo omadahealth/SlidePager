58310041e668517abfbd454ce140d2d0ebc9405f
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2015-04-02 11:44:28 -0700

    updated the circularbarpager lib to allow circle fill and start arrow disabling

---
+++ app/src/main/java/com/github/omadahealth/slidepager/DayProgressView.java
@@ -0,0 +1,24 @@
+package com.github.omadahealth.slidepager;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by stoyan on 4/2/15.
+ */
+public class DayProgressView extends RelativeLayout {
+    
+    
+    public DayProgressView(Context context) {
+        super(context);
+    }
+
+    public DayProgressView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DayProgressView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
